d1f760d1f9199aa6fd54ffc4a037fd6bec46a3fe
Francesco Di Pinto <fdipinto2003@gmail.com>
2025-07-14 19:23:24 +0200

    Update ScriptDB.sql

--- ScriptDB.sql
+++ ScriptDB.sql
@@ -3,7 +3,8 @@
 -- Host: localhost    Database: sistema_gestionale_di_task_didattici_con_badge
 -- ------------------------------------------------------
 -- Server version	8.0.33
-
+CREATE DATABASE IF NOT EXISTS sistema_gestionale_di_task_didattici_con_badge;
+USE sistema_gestionale_di_task_didattici_con_badge;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
